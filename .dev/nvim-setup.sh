275edf23ca1cf14f1b0e3bb9c1952a7db48b3935
Vinicius Mayrink <vmayrink12@gmail.com>
2024-05-02 15:03:18 -0300

    Adding pylsp config

--- .dev/nvim-setup.sh
+++ .dev/nvim-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Installing neovim dependecies (neovim, git)..."
-apk add neovim git build-base
+apk add neovim git build-base wget
+pip install python-lsp-server
 
 echo "Installing packer.vim..."
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim || {
@@ -14,10 +15,8 @@ if [ -d $neovim_config_path ]; then
   rm -rf $neovim_config_path
 fi
 git clone --depth 1 https://github.com/vncsmyrnk/vim-config-python.git $neovim_config_path
-source $neovim_config_path/*/*/*.lua
 
 echo "neovim is installed and configured"
 
 exit 0
-
 
